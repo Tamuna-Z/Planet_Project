713e975a391db110c7be008a5266096b0e3079b2
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2023-04-15 18:19:52 +0400

    add background

--- src/components/contents/Content.tsx
+++ src/components/contents/Content.tsx
@@ -1,12 +1,19 @@
+import { useParams } from "react-router-dom";
 import Header from "../header/Header";
 import{Main} from "./Content.styled";
-import React from 'react';
+import React, { useState } from 'react';
+// import Info from "../../info/Info";
 
 export default function Content(props:any){
-    // const planets= props.data.find((planet:any) => planet.name);
+    const {name}=useParams();
+    const planets= props.data.find((planet:any) => planet.name === name);
+    const [surface, setSurface]=useState(false);
+    const[display,setDisplay]=useState<any>(false);
+    const[image,setImage]=useState(planets.images.planet);
+    const[text,setText]=useState(planets.overview.content);
     return(
         <Main>
-            <Header></Header>
+            <Header setDisplay ={setDisplay} display ={display}/>
 
         </Main>
 
